efe105a4f75c8aaab63c1f139a6a4cacb2d6f931
Jacob Wallace <jacob.wallace@utexas.edu>
2017-03-20 13:05:48 -0500

    initial files

---
+++ assets/javascript/logic.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+var config = {
+    apiKey: "AIzaSyAOFi3bIryWbgOrsTHyoS3bTKzTGQOpcHs",
+    authDomain: "thecodingbayshop.firebaseapp.com",
+    databaseURL: "https://thecodingbayshop.firebaseio.com",
+    storageBucket: "thecodingbayshop.appspot.com",
+    messagingSenderId: "648124109917"
+  };
+  firebase.initializeApp(config);
+
+var database = firebase.database();
+
+});
